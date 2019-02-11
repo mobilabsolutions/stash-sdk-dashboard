7c152a6c81bb90a0391b5005fc3719130e1717dc
Christoph Stock <stockulus@icloud.com>
2019-02-11 11:13:17 +0100

    add tpes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import { ThemeProvider } from 'styled-components'
 import { theme, GlobalStyle } from '../assets/style'
 import { NextContextProvider } from '../hooks/use_next_context'
 
-export default class extends App {
+type Props = {
+  context: object
+}
+
+export default class extends App<Props> {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {}
 
@@ -15,6 +19,7 @@ export default class extends App {
 
     return {
       pageProps,
+      Component,
       context: {
         pathname: ctx.pathname,
         query: ctx.query,
