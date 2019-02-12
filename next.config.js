ff43b5984e3b7ed9603db618e2604e2ad53acf53
Christoph Stock <stockulus@icloud.com>
2019-02-12 10:04:49 +0100

    update dependencies / move to next 8

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+
+module.exports = withTypescript({
+  onDemandEntries: {
+    maxInactiveAge: 1000 * 60 * 60, // 1h
+    pagesBufferLength: 10
+  }
+})
