9c5e03200b737eb7757013a5bad6aea91a746185
Christoph Stock <stockulus@icloud.com>
2019-02-27 16:54:28 +0100

    update server

--- server/index.ts
+++ server/index.ts
@@ -2,52 +2,23 @@ process.env.TZ = 'UTC'
 
 require('isomorphic-fetch')
 
-const fs = require('fs')
 const fastify = require('fastify')
 const proxy = require('fastify-http-proxy')
 const Next = require('next')
 
+const prepareDatepickerCss = require('./prepare_datepicker_css').default
+
 const PORT = parseInt(process.env.PORT, 10) || 3000
+const API_UPSTREAM = process.env.API_UPSTREAM || 'https://pd.mblb.net'
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
 const BIND_ADDRESS = IS_DEVELOPMENT ? '127.0.0.1' : '0.0.0.0'
-const API_UPSTREAM = process.env.API_UPSTREAM || 'https://pd.mblb.net'
 
 const nextApp = Next({ dev: IS_DEVELOPMENT })
 const handle = nextApp.getRequestHandler()
 
-const prepareDatepicker = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      './node_modules/react-dates/lib/css/_datepicker.css',
-      'utf8',
-      (error, data) => {
-        if (error) return reject(error)
-
-        resolve(
-          data
-            .replace('#b2f1ec', '#CEFBF9')
-            .replace('#80e8e0', '#9DF6F2')
-            .replace('#66e2da', '#6BF2EC')
-            .replace('#33dacd', '#08E8DE')
-            .replace('#00a699', '#07D0C7')
-            .replace('#008489', '#048B85')
-            .replace('#007a87', '#035C58')
-            .replace('#f2f2f2', '#F6F7F7')
-            .replace('#e4e7e7', '#EDEFF0')
-            .replace('#dbdbdb', '#D1D5D7')
-            .replace('#cacccd', '#A3AAAF')
-            .replace('#82888a', '#747F86')
-            .replace('#757575', '#46545E')
-            .replace('#484848', '#22333F')
-        )
-      }
-    )
-  })
-}
-
 nextApp
   .prepare()
-  .then(() => prepareDatepicker())
+  .then(() => prepareDatepickerCss())
   .then(datepickerCss => {
     const server = fastify()
 
---
+++ server/prepare_datepicker_css.ts
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+export default () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      './node_modules/react-dates/lib/css/_datepicker.css',
+      'utf8',
+      (error: Error, data: string) => {
+        if (error) return reject(error)
+
+        resolve(
+          data
+            .replace('#b2f1ec', '#CEFBF9')
+            .replace('#80e8e0', '#9DF6F2')
+            .replace('#66e2da', '#6BF2EC')
+            .replace('#33dacd', '#08E8DE')
+            .replace('#00a699', '#07D0C7')
+            .replace('#008489', '#048B85')
+            .replace('#007a87', '#035C58')
+            .replace('#f2f2f2', '#F6F7F7')
+            .replace('#e4e7e7', '#EDEFF0')
+            .replace('#dbdbdb', '#D1D5D7')
+            .replace('#cacccd', '#A3AAAF')
+            .replace('#82888a', '#747F86')
+            .replace('#757575', '#46545E')
+            .replace('#484848', '#22333F')
+        )
+      }
+    )
+  })
+}
