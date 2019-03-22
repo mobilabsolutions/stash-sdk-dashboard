562b3c6d1a30a18db4c74abf453bfff3fb5dd662
Christoph Stock <stockulus@icloud.com>
2019-03-22 13:24:15 +0100

    refactor + better form handling

--- components/organisms/keys_configuration/index.tsx
+++ components/organisms/keys_configuration/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Formik, Field } from 'formik'
 
-import styled from '../../styled'
 import {
   H3,
   KeyIcon,
@@ -16,83 +15,12 @@ import { Popup, Input } from '../../molecules'
 import { useLocalization, useToast } from '../../../hooks'
 import PageForm from '../page_form'
 import copyToClipboard from './copy_to_clipboard'
-
-const KeysWrapper = styled.div`
-  margin-top: 16px;
-  margin-bottom: 16px;
-`
-
-const PublicKeyWrapper = styled.div`
-  margin-top: 16px;
-  margin-bottom: 16px;
-  display: flex;
-  align-items: center;
-  > span {
-    margin-left: 24px;
-    margin-right: 24px;
-    display: block;
-    min-width: 265px;
-  }
-  > svg:not(:first-child) {
-    cursor: pointer;
-  }
-  > svg:last-child {
-    margin-left: 24px;
-  }
-`
-
-const PopupWrapper = styled.div`
-  padding: 16px;
-  > .buttons {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-  }
-  > .extra-padding {
-    padding-top: 32px;
-  }
-  > .buttons > button {
-    margin-left: 16px;
-  }
-  > h3 {
-    display: block;
-    max-width: 350px;
-  }
-`
-
-const PrivateKeyWrapper = styled.div`
-  margin-top: 16px;
-  margin-bottom: 16px;
-  display: flex;
-  align-items: center;
-  .name,
-  .key {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  .name > span,
-  .key > span {
-    margin-left: 24px;
-    margin-right: 24px;
-    display: block;
-    min-width: 265px;
-  }
-  .name > svg,
-  .key > svg {
-    cursor: pointer;
-    margin-left: 24px;
-  }
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-  }
-  .instruction {
-    display: block;
-    margin-left: 24px;
-  }
-`
+import {
+  PrivateKeyWrapper,
+  PublicKeyWrapper,
+  PopupWrapper,
+  KeysWrapper
+} from './styled'
 
 interface KeyEntity {
   id: number
@@ -230,33 +158,33 @@ export default function KeysConfiguration({
             setIsCreating(false)
           }}
           render={props => (
-            <PopupWrapper>
-              <Field
-                name="name"
-                render={({ field, form }) => (
-                  <>
-                    <H3>{getText('Name')}</H3>
-                    <Input
-                      field={field}
-                      form={form}
-                      placeholder={getText('Name')}
-                    />
-                  </>
-                )}
-              />
-              <div className="buttons">
-                <SecondaryButton
-                  label={getText('Cancel')}
-                  onClick={() => setIsCreating(false)}
+            <form onSubmit={props.handleSubmit}>
+              <PopupWrapper>
+                <Field
+                  name="name"
+                  render={({ field, form }) => (
+                    <>
+                      <H3>{getText('Name')}</H3>
+                      <Input
+                        field={field}
+                        form={form}
+                        placeholder={getText('Name')}
+                      />
+                    </>
+                  )}
                 />
-                <PrimaryButton
-                  label={getText('Create')}
-                  type="button"
-                  disabled={!props.isValid}
-                  onClick={() => props.submitForm()}
-                />
-              </div>
-            </PopupWrapper>
+                <div className="buttons">
+                  <SecondaryButton
+                    label={getText('Cancel')}
+                    onClick={() => setIsCreating(false)}
+                  />
+                  <PrimaryButton
+                    label={getText('Create')}
+                    disabled={!props.isValid}
+                  />
+                </div>
+              </PopupWrapper>
+            </form>
           )}
         />
       </Popup>
---
+++ components/organisms/keys_configuration/styled.tsx
@@ -0,0 +1,78 @@
+import styled from '../../styled'
+
+export const KeysWrapper = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+`
+
+export const PublicKeyWrapper = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  > span {
+    margin-left: 24px;
+    margin-right: 24px;
+    display: block;
+    min-width: 265px;
+  }
+  > svg:not(:first-child) {
+    cursor: pointer;
+  }
+  > svg:last-child {
+    margin-left: 24px;
+  }
+`
+
+export const PopupWrapper = styled.div`
+  padding: 16px;
+  > .buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  > .extra-padding {
+    padding-top: 32px;
+  }
+  > .buttons > button {
+    margin-left: 16px;
+  }
+  > h3 {
+    display: block;
+    max-width: 350px;
+  }
+`
+
+export const PrivateKeyWrapper = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  .name,
+  .key {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .name > span,
+  .key > span {
+    margin-left: 24px;
+    margin-right: 24px;
+    display: block;
+    min-width: 265px;
+  }
+  .name > svg,
+  .key > svg {
+    cursor: pointer;
+    margin-left: 24px;
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+  .instruction {
+    display: block;
+    margin-left: 24px;
+  }
+`
