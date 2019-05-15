e3337665830b4c55a265abf6b71769c55ec4ccf4
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-05-15 13:13:17 +0200

    Formating and amount will return more data about the currency

--- hooks/use_localization/index.test.tsx
+++ hooks/use_localization/index.test.tsx
@@ -2,6 +2,7 @@
 
 import { useLocalization } from './index'
 import { testRender } from '../../test_utils'
+import localizedAmount from './localized_amount'
 
 const PaymentDashboard = () => {
   const { getText } = useLocalization()
@@ -22,3 +23,17 @@ const Number = () => {
 it('Should "formatNumber" in "en"', () => testRender(Number))
 it('Should "formatNumber" in "de"', () =>
   testRender(Number, null, { locale: 'de' }))
+
+const Amount = () => {
+  const { formatAmount } = useLocalization()
+
+  return <span>{formatAmount('EUR', 12345).value}</span>
+}
+it('Should "formatAmount" in "en"', () => testRender(Amount))
+it('Should "formatAmount" in "de"', () =>
+  testRender(Amount, null, { locale: 'de' }))
+
+it('"localizedAmount" should return all properties for a currency', () => {
+  expect(localizedAmount('EUR', 12345, 'de')).toMatchSnapshot()
+  expect(localizedAmount('EUR', 12345, 'en')).toMatchSnapshot()
+})
