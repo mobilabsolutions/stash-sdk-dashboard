8837738841201f784c82e96a7f95bfdb411ff53a
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-05-15 13:21:48 +0200

    New popup actions.

--- components/templates/transactions/popup.tsx
+++ components/templates/transactions/popup.tsx
@@ -1,139 +1,56 @@
-import { useState } from 'react'
-
 import { useLocalization } from '../../../hooks'
-import { PrimaryButton, SecondaryButton, WarnButton } from '../../atoms'
-import { Popup } from '../../molecules'
-import styled from '../../styled'
+import { WarnPopup, ActionPopup } from '../../molecules'
+import { RefundForm } from '../../organisms'
 
-const PopupContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
+export default ({
+  onClose,
+  action,
+  show,
+  currencyId,
+  initialRefund,
+  onAction
+}) => {
+  const { getText } = useLocalization()
 
-const ContentContainer = styled.div`
-  padding: 32px;
-`
-
-const ValueContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 12px 0 12px 0;
-`
-
-const Label = styled.label`
-  color: ${props => props.theme.shade.A600};
-  font-family: ${props => props.theme.font};
-  font-size: 1.2em;
-  width: 10em;
-  flex: 0 0 35%;
-`
-
-const Value = styled.span`
-  color: ${props => props.theme.shade.A800};
-  font-family: ${props => props.theme.font};
-  font-size: 1.2em;
-  font-weight: bold;
-  width: 10em;
-  flex: 0 0 60%;
-`
-
-const ButtonContainer = styled.div`
-  background-color: ${props => props.theme.shade.A25};
-  display: flex;
-  flex-direction: row;
-  padding: 12px 24px 12px 24px;
-  align-items: center;
-  justify-content: flex-end;
-  border-radius: 5px;
-  > button {
-    margin-left: 12px;
-  }
-`
-
-export default ({ detail, onClose, onRefund, isRefunding }) => {
-  const { getText, formatDate, formatAmount } = useLocalization()
-  const [inRefund, setInRefund] = useState(false)
-
-  const getContent = () => {
-    if (inRefund) {
-      return (
-        <PopupContainer>
-          <ContentContainer>
-            <Value>
-              {getText(
-                'Do you really want to Refund %{amount} for the Transaction %{id}?',
-                {
-                  id: detail.transactionId,
-                  amount: formatAmount(detail.currency, detail.amount)
-                }
-              )}
-            </Value>
-          </ContentContainer>
-          <ButtonContainer>
-            <SecondaryButton
-              label={getText('Cancel')}
-              onClick={() => {
-                setInRefund(false)
-                onClose()
-              }}
-            />
-            <WarnButton
-              label={getText('Refund')}
-              onClick={() => {
-                onRefund(detail.transactionId).then(() => onClose())
-              }}
-              disabled={isRefunding}
-            />
-          </ButtonContainer>
-        </PopupContainer>
-      )
+  const getHeader = _action => {
+    switch (_action) {
+      case 'reverse':
+        return getText('Are you sure you want to reverse the transaction?')
+      case 'capture':
+        return getText('Are you sure you want to capture this transaction?')
+      case 'refund':
+        return getText('Refund Payment')
+      default:
+        return ''
     }
+  }
 
+  if (action === 'capture') {
     return (
-      <PopupContainer>
-        <ContentContainer>
-          <ValueContainer>
-            <Label>{getText('ID')}</Label>
-            <Value>{detail.transactionId}</Value>
-          </ValueContainer>
-          <ValueContainer>
-            <Label>{getText('Status')}</Label>
-            <Value>{detail.status}</Value>
-          </ValueContainer>
-          <ValueContainer>
-            <Label>{getText('Text')}</Label>
-            <Value>{detail.reason}</Value>
-          </ValueContainer>
-          <ValueContainer>
-            <Label>{getText('CustomerID')}</Label>
-            <Value>{detail.customerId}</Value>
-          </ValueContainer>
-          <ValueContainer>
-            <Label>{getText('Timestamp')}</Label>
-            <Value>{formatDate(detail.timestamp)}</Value>
-          </ValueContainer>
-          <ValueContainer>
-            <Label>{getText('Amount')}</Label>
-            <Value>{formatAmount(detail.currency, detail.amount)}</Value>
-          </ValueContainer>
-        </ContentContainer>
-        <ButtonContainer>
-          {detail.status === 'approved' && (
-            <SecondaryButton
-              label={getText('Refund')}
-              onClick={() => setInRefund(true)}
-            />
-          )}
-          <PrimaryButton label={getText('Close')} onClick={onClose} />
-        </ButtonContainer>
-      </PopupContainer>
+      <ActionPopup show={show} onClose={onClose} header={getHeader(action)}>
+        <RefundForm
+          onCancel={onClose}
+          currencyId={currencyId}
+          initialRefund={initialRefund}
+          onSubmit={values => {
+            onAction(action, values)
+          }}
+        />
+      </ActionPopup>
     )
   }
 
   return (
-    <Popup show={!!detail} onClose={onClose}>
-      {!!detail && getContent()}
-    </Popup>
+    <WarnPopup
+      show={show}
+      onClose={onClose}
+      onAction={() => {
+        onAction(action)
+      }}
+      header={getHeader(action)}
+      action={getText(action)}
+    >
+      {null}
+    </WarnPopup>
   )
 }
