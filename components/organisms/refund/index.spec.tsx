0a4bff342bdbe72664c79788acdf51e98add5c78
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-05-23 13:38:33 +0200

    New tests and testing library

--- components/organisms/refund/index.spec.tsx
+++ components/organisms/refund/index.spec.tsx
@@ -1,6 +1,7 @@
-import { testRender } from '../../../test_utils'
+import { testRender, deepRender } from '../../../test_utils'
 // import { shallow, mount } from 'enzyme'
 import RefundForm from './index'
+import { fireEvent } from 'react-testing-library'
 
 it('RefundForm Should Render', () =>
   testRender(RefundForm, {
@@ -14,14 +15,31 @@ it('RefundForm Should use loading status', () =>
     isLoading: true,
     currencyId: 'EUR'
   }))
-// it('RefundForm Should disable amount initially', () => {
-//   const form = mount(<RefundForm initialRefund={12345} currencyId={'EUR'} />)
-//   const RefundInput = form.find('[name="refund"]').get(0)
-//   expect(RefundInput.props.disabled).toBe(true)
-// })
 
-// it('RefundForm Should disable amount only when full refund is picked', () => {
-//   const form = mount(<RefundForm initialRefund={12345} currencyId={'EUR'} />)
-//   const RefundInput = form.find('[name="refund"]').get(0)
-//   expect(RefundInput.props.disabled).toBe(true)
-// })
+it('RefundForm Should disable amount initially', () => {
+  const { getByPlaceholderText } = deepRender(RefundForm, {
+    initialRefund: 12345,
+    currencyId: 'EUR'
+  })
+
+  const RefundInput = getByPlaceholderText('Refund')
+  expect(RefundInput.getAttribute('disabled')).toBeDefined()
+})
+
+it('RefundForm Should disable amount only when full refund is picked', () => {
+  const { getByPlaceholderText, getByLabelText } = deepRender(RefundForm, {
+    initialRefund: 12345,
+    currencyId: 'EUR'
+  })
+
+  fireEvent(
+    getByLabelText('Partial'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  )
+
+  const RefundInput = getByPlaceholderText('Refund')
+  expect(RefundInput.getAttribute('disabled')).toBeNull()
+})
