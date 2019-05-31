5c8b8bc42e62c659f6142a989d5f99fb0da5bc93
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-05-31 11:35:23 +0200

    Input search component

--- components/molecules/input_search/index.tsx
+++ components/molecules/input_search/index.tsx
@@ -1,5 +1,8 @@
 import styled from '../../styled'
-import { forwardRef, useRef, useState } from 'react'
+import { forwardRef, useRef, useState, ChangeEvent, useEffect } from 'react'
+import { useDebounce } from '../../../hooks'
+import { Magnify } from '../../../components/atoms'
+import InputWrapperProps from '../../../components/atoms/input/interfaces'
 
 const HtmlInput = styled.input`
   border: none;
@@ -11,41 +14,83 @@ const HtmlInput = styled.input`
   margin-right: 16px;
   box-shadow: none;
   width: 100%;
+  background-color: ${props => props.theme.shade.A25};
   :focus {
     outline: none;
   }
   ::placeholder {
-    color: ${props => props.theme.shade.A100};
+    color: ${props => props.theme.shade.A200};
   }
   ::selection {
     background-color: ${props => props.theme.primary.A100};
   }
 `
 
+const InputWrapper = styled.div<InputWrapperProps>`
+  display: flex;
+  flex-direction: row;
+  border-radius: 4px;
+  min-height: 38px;
+  border: solid 1px
+    ${props =>
+      props.focused ? props.theme.primary.A600 : props.theme.shade.A50};
+  background-color: ${props => props.theme.shade.A25};
+  :hover {
+    border-color: ${props =>
+      props.focused ? props.theme.primary.A600 : props.theme.shade.A200};
+  }
+`
+
 function TheInput(
   {
-    field: { name, value, onChange, onBlur },
-    form: { touched, errors },
+    field: { name, onChange },
     placeholder = 'Insert text',
     title = '',
-    className = '',
+    initialValue = '',
     disabled = false,
     autoFocus = false
   },
   inputRef: any
 ) {
-  const [focused, setFocused] = useState(autoFocus)
   const localRef = useRef(undefined)
+  const [focused, setFocused] = useState(autoFocus)
 
   const ref = inputRef || localRef
 
-  const handleClick = () => inputRef && inputRef.current.focus()
-  const hasErrors = touched[name] && errors[name]
+  const [value, setValue] = useState(initialValue)
+
+  const debouncedSearchTerm = useDebounce(value, 500)
+  useEffect(() => {
+    onChange(debouncedSearchTerm)
+  }, [debouncedSearchTerm])
+
+  function _onChange(ev: ChangeEvent<HTMLInputElement>) {
+    setValue(ev.target.value)
+  }
 
   return (
-    <div>
-      <HtmlInput />
-    </div>
+    <InputWrapper focused={focused} hasErrors={false}>
+      <Magnify
+        style={{ margin: 'auto', paddingLeft: '8px' }}
+        height={24}
+        width={24}
+      />
+      <HtmlInput
+        ref={ref}
+        name={name}
+        value={value}
+        disabled={disabled}
+        title={title}
+        type="text"
+        placeholder={placeholder}
+        onFocus={() => setFocused(true)}
+        onBlur={() => {
+          setFocused(false)
+        }}
+        onChange={_onChange}
+        autoFocus={autoFocus}
+      />
+    </InputWrapper>
   )
 }
 
