874dad77afca69a4f4b7ae9633bf3c21114fda7a
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-06-04 14:40:26 +0200

    Adding check to selected option on select

--- components/molecules/select/index.tsx
+++ components/molecules/select/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
-import ReactSelect from 'react-select'
+import ReactSelect, { components } from 'react-select'
 import Theme from '../../../assets/style/theme'
+import styled from '../../styled'
 
+const Checkmark = styled.span`
+  text-align: right;
+  bottom: 5px;
+  display: block;
+  margin: auto;
+  float: right;
+  width: 22px;
+  height: 22px;
+  transform: rotate(45deg);
+`
+const Checkmark_stem = styled.span`
+  position: absolute;
+  width: 3px;
+  height: 9px;
+  background-color: ${props => props.theme.primary.A600};
+  left: 11px;
+  top: 6px;
+`
+const Checkmark_kick = styled.span`
+  position: absolute;
+  width: 3px;
+  height: 3px;
+  background-color: ${props => props.theme.primary.A600};
+  left: 8px;
+  top: 12px;
+`
+const Check = () => (
+  <Checkmark>
+    <Checkmark_stem />
+    <Checkmark_kick />
+  </Checkmark>
+)
+
+const Option = props => {
+  return (
+    <components.Option {...props}>
+      {props.children}
+      {props.isSelected && <Check />}
+    </components.Option>
+  )
+}
 const global = {
   color: Theme.shade.A200,
   fontSize: '14px'
@@ -31,19 +73,26 @@ const styles = {
     ...propv,
     backgroundColor: Theme.shade.A25,
     borderColor: isFocused ? Theme.primary.A600 : Theme.shade.A50,
-    boxShadow: isFocused ? `0 0 0 1px ${Theme.primary.A600}` : propv.boxShadow
+    '&:hover': {
+      borderColor: isFocused ? Theme.primary.A600 : Theme.shade.A200
+    },
+    boxShadow: 'unset'
   }),
-  option: (prov, { isSelected }) => ({
+  option: (prov, { isSelected, isFocused }) => ({
     ...prov,
     ...global,
     borderBottom: `1px solid ${Theme.shade.A25}`,
     textTransform: 'capitalize',
-    backgroundColor: isSelected ? Theme.primary.A50 : prov.backgroundColor,
+    backgroundColor: isSelected
+      ? Theme.primary.A50
+      : isFocused
+      ? Theme.shade.A25
+      : prov.backgroundColor,
     color: isSelected ? Theme.primary.A600 : Theme.shade.A200,
     fontweight: 500
   })
 }
 
 export default function Select(props) {
-  return <ReactSelect {...props} styles={styles} />
+  return <ReactSelect {...props} styles={styles} components={{ Option }} />
 }
