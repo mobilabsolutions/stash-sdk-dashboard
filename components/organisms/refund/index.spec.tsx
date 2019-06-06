6faf68450bc17167d9584adbfe801f5983f4921b
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-06-06 13:37:56 +0200

    Reset amout when full refound is picked

--- components/organisms/refund/index.spec.tsx
+++ components/organisms/refund/index.spec.tsx
@@ -43,3 +43,31 @@ it('RefundForm Should disable amount only when full refund is picked', () => {
   const RefundInput = getByPlaceholderText('Refund')
   expect(RefundInput.getAttribute('disabled')).toBeNull()
 })
+
+it('RefundForm Should reset amount if full refund is picked', () => {
+  const { getByPlaceholderText, getByLabelText } = deepRender(RefundForm, {
+    initialRefund: 12345,
+    currencyId: 'EUR'
+  })
+
+  fireEvent(
+    getByLabelText('Partial'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  )
+  const InputEl = getByPlaceholderText('Refund')
+  fireEvent.change(InputEl, { target: { value: '€35.00' } })
+  expect(InputEl.getAttribute('value')).toBe('€35.00')
+
+  fireEvent(
+    getByLabelText('Full'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  )
+
+  expect(InputEl.getAttribute('value')).toBe('€12,345.00')
+})
