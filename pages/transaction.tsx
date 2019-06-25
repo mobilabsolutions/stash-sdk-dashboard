12567f228ada06707c2e152cc7da568bc780f57d
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-06-25 17:20:27 +0200

    Conditional rendering for details

--- pages/transaction.tsx
+++ pages/transaction.tsx
@@ -39,50 +39,46 @@ const TransactionDetails = ({ router }: DetailProps) => {
           <BackButton>{getText('Back to transactions overview')}</BackButton>
         </Link>
         {details && (
-          <DetailItem>
-            <TransactionEssentials
-              refund={refund}
-              reverse={reverse}
-              capture={capture}
-              transactionId={details.transactionId}
-              amount={details.amount}
-              currency={details.currencyId}
-              status={details.status}
-              action={details.action}
-              date={details.createdDate}
-              extra={details.paymentInfo.extra}
-            />
-          </DetailItem>
-        )}
-        {details && (
-          <DetailItem>
-            <TransactionTimeline timeline={details.timelineInfo} />
-          </DetailItem>
-        )}
-        {details && (
-          <DetailItem>
-            <TransactionDetailCmp
-              transactionId={details.transactionId}
-              action={details.action}
-              status={details.status}
-              amount={details.amount}
-              currency={details.currencyId}
-              description={details.reason}
-              createdDate={details.createdDate}
-            />
-          </DetailItem>
-        )}
-        {details && (
-          <DetailItem>
-            <PaymentMethodDetails
-              paymentMethod={details.paymentMethod}
-              ccConfig={details.paymentInfo.extra.ccConfig}
-              payPalConfig={details.paymentInfo.extra.payPalConfig}
-              personalData={details.paymentInfo.extra.personalData}
-              sepaConfig={details.paymentInfo.extra.sepaConfig}
-              aliasId={details.aliasId}
-            />
-          </DetailItem>
+          <>
+            <DetailItem>
+              <TransactionEssentials
+                refund={refund}
+                reverse={reverse}
+                capture={capture}
+                transactionId={details.transactionId}
+                amount={details.amount}
+                currency={details.currencyId}
+                status={details.status}
+                action={details.action}
+                date={details.createdDate}
+                extra={details.paymentInfo.extra}
+              />
+            </DetailItem>
+            <DetailItem>
+              <TransactionTimeline timeline={details.timelineInfo} />
+            </DetailItem>
+            <DetailItem>
+              <TransactionDetailCmp
+                transactionId={details.transactionId}
+                action={details.action}
+                status={details.status}
+                amount={details.amount}
+                currency={details.currencyId}
+                description={details.reason}
+                createdDate={details.createdDate}
+              />
+            </DetailItem>
+            <DetailItem>
+              <PaymentMethodDetails
+                paymentMethod={details.paymentMethod}
+                ccConfig={details.paymentInfo.extra.ccConfig}
+                payPalConfig={details.paymentInfo.extra.payPalConfig}
+                personalData={details.paymentInfo.extra.personalData}
+                sepaConfig={details.paymentInfo.extra.sepaConfig}
+                aliasId={details.aliasId}
+              />
+            </DetailItem>
+          </>
         )}
       </CustomScrollContainer>
     </Page>
