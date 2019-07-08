f6a1c27e8685deb184f31faa136de97e10e2d6c6
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-07-08 11:25:08 +0200

    Hook to download CSV

---
+++ hooks/use_download_csv/index.tsx
@@ -0,0 +1,33 @@
+import FileSaver from 'file-saver'
+import { useApi } from '../use_api'
+import { useState } from 'react'
+
+const getInitial = () => ({
+  downloading: false,
+  error: false
+})
+
+const type = 'text/csv;charset=ISO-8859-1'
+
+export default (url: string, filename: string) => {
+  const [state, setstate] = useState(getInitial())
+  const { getRaw } = useApi()
+
+  async function download() {
+    setstate({ downloading: true, error: false })
+    try {
+      const { result } = await getRaw(url)
+      const text = await result.text()
+      var blob = new Blob([text], { type })
+      FileSaver.saveAs(blob, filename)
+      setstate({ downloading: false, error: false })
+    } catch (e) {
+      setstate({ downloading: false, error: true })
+    }
+  }
+
+  return {
+    ...state,
+    download
+  }
+}
