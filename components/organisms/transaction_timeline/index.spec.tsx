626adf42150b0c167ac4b1befcd5437e26a60f30
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-07-15 10:59:25 +0200

    New timeline design

--- components/organisms/transaction_timeline/index.spec.tsx
+++ components/organisms/transaction_timeline/index.spec.tsx
@@ -1,7 +1,7 @@
 import { testRender } from '../../../test_utils'
 import TransactionTimeline from './index'
 
-it('CaptureForm Should Render', () =>
+it('TransactionTimeline Should Render', () =>
   testRender(TransactionTimeline, {
     timeline: [
       {
@@ -18,17 +18,18 @@ it('CaptureForm Should Render', () =>
         reason: 'Test Payment',
         status: 'SUCCESS'
       }
-    ]
+    ],
+    currencyId: 'EUR'
   }))
 
-it('CaptureForm Should Render preauth explaination', () =>
+it('TransactionTimeline Should ommit empty reasons', () =>
   testRender(TransactionTimeline, {
     timeline: [
       {
-        action: 'PREAUTH',
+        action: 'REFUND',
         amount: 3470,
         createdDate: '2019-06-26 14:49:18',
-        reason: 'No se',
+        reason: '',
         status: 'SUCCESS'
       },
       {
@@ -36,7 +37,8 @@ it('CaptureForm Should Render preauth explaination', () =>
         amount: 3470,
         createdDate: '2019-06-21 17:18:16',
         reason: 'Test Payment',
-        status: 'FAIL'
+        status: 'SUCCESS'
       }
-    ]
+    ],
+    currencyId: 'EUR'
   }))
