984bc8fa6c9315cd408dced68c9d70953cf0bc0a
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-07-17 16:24:55 +0200

    Types for PSPs

---
+++ types/psp.ts
@@ -0,0 +1,23 @@
+export enum PspType {
+  BS_PAYONE = 'BS_PAYONE',
+  ADYEN = 'ADYEN',
+  BRAINTREE = 'BRAINTREE'
+}
+
+export interface PSP {
+  accountId?: number
+  country?: string
+  currency?: string
+  default?: boolean
+  key?: number
+  locale?: string
+  merchantId?: number
+  portalId?: number
+  privateKey?: string
+  publicKey?: string
+  sandboxMerchantId?: number
+  sandboxPrivateKey?: string
+  sandboxPublicKey?: string
+  type: PspType
+  urlPrefix?: string
+}
