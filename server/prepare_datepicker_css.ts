102774e37c4c962d19b5783d093e8b603980a0c0
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-07-30 11:12:13 +0200

    Fix deploy error

--- server/prepare_datepicker_css.ts
+++ server/prepare_datepicker_css.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs'
-import { theme } from '../assets/style'
 
 export default () => {
   return new Promise<string>((resolve, reject) => {
@@ -11,20 +10,20 @@ export default () => {
 
         resolve(
           data
-            .replace(/#b2f1ec/g, theme.primary.A100)
-            .replace(/#80e8e0/g, theme.primary.A200)
-            .replace(/#66e2da/g, theme.primary.A300)
-            .replace(/#33dacd/g, theme.primary.A500)
-            .replace(/#00a699/g, theme.primary.A500)
-            .replace(/#008489/g, theme.primary.A700)
-            .replace(/#007a87/g, theme.primary.A800)
-            .replace(/#f2f2f2/g, theme.shade.A25)
-            .replace(/#e4e7e7/g, theme.shade.A50)
-            .replace(/#dbdbdb/g, theme.shade.A100)
-            .replace(/#cacccd/g, theme.shade.A200)
-            .replace(/#82888a/g, theme.shade.A300)
-            .replace(/#757575/g, theme.shade.A400)
-            .replace(/#484848/g, theme.shade.A500)
+            .replace(/#b2f1ec/g, '#d8e7fd')
+            .replace(/#80e8e0/g, '#b7d2fb')
+            .replace(/#66e2da/g, '#8ebaf8')
+            .replace(/#33dacd/g, '#2e7ef2')
+            .replace(/#00a699/g, '#2e7ef2')
+            .replace(/#008489/g, '#1f56a7')
+            .replace(/#007a87/g, '#194584')
+            .replace(/#f2f2f2/g, '#f7fafe')
+            .replace(/#e4e7e7/g, '#eff5fe')
+            .replace(/#dbdbdb/g, '#d8e7fd')
+            .replace(/#cacccd/g, '#b7d2fb')
+            .replace(/#82888a/g, '#8ebaf8')
+            .replace(/#757575/g, '#609df6')
+            .replace(/#484848/g, '#2e7ef2')
             .replace('font-size: 19px;', 'font-size: 14px;')
         )
       }
