bfa8cb69daf85f1803d6d96c5b25e2e9403c7a83
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-08-15 16:16:17 +0200

    Hook for getting activity for the selected date

---
+++ hooks/use_day_activity/index.ts
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import { TodaysActivity as Activity } from '../../types'
+import { useApi } from '../'
+import { Moment, defaultFormatUtc } from 'moment'
+
+interface ActivityState {
+  activities: Activity[]
+  loading: boolean
+  error: any
+}
+
+interface ApiResponse {
+  transactions: {
+    amount: number
+    hour: number
+  }[]
+}
+
+function initialValues(): ActivityState {
+  return {
+    activities: [],
+    loading: false,
+    error: null
+  }
+}
+
+export default (date: Moment) => {
+  const [state, setState] = useState(initialValues())
+  const { get, merchantId } = useApi()
+  const load = () => {
+    setState(prevState => ({ ...prevState, loading: true }))
+    get(
+      `/api/v1/home/${encodeURIComponent(merchantId)}/activity?date=${date
+        .clone()
+        .hours(0)
+        .minutes(0)
+        .format(defaultFormatUtc)}`
+    )
+      .then((response: { result: ApiResponse }) => {
+        const { transactions = [] } = response.result
+
+        return setState({
+          activities: transactions.map(trans => ({
+            time: trans.hour.toString(),
+            amount: trans.amount
+          })),
+          loading: false,
+          error: null
+        })
+      })
+      .catch((error: any) => {
+        setState(prev => ({ ...prev, loading: false, error }))
+      })
+  }
+
+  useEffect(load, [merchantId, get])
+
+  return {
+    ...state,
+    load
+  }
+}
