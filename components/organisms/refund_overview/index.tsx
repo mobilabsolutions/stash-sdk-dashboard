1cb6391ca8b421b3ed3ee6a79b39f907e580bffb
Reinaldo Viera <reinaldo@Reinaldos-MacBook-Pro.local>
2019-08-22 14:11:07 +0200

    Deprecation warning

--- components/organisms/refund_overview/index.tsx
+++ components/organisms/refund_overview/index.tsx
@@ -23,7 +23,7 @@ const ActivityContainer = styled.div`
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => ({
   time: moment()
-    .add('days', -n)
+    .add(-n, 'days')
     .unix(),
   amount: Math.floor(Math.random() * 100 * 34)
 }))
