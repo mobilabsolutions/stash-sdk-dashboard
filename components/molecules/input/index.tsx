ecf15e3e52bc68f2ac9b1331f46064fb5beaf0ca
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-09-25 10:38:48 +0200

    Input component for filters

--- components/molecules/input/index.tsx
+++ components/molecules/input/index.tsx
@@ -8,6 +8,7 @@ import {
   InputErrorMessage
 } from '../../atoms'
 import styled from '../../styled'
+import { ReactComponentLike } from 'prop-types'
 
 const InputChildren = props => {
   return <Input {...props} />
@@ -19,62 +20,71 @@ const InputErrorWrapper = styled.div`
   width: 100%;
 `
 
-function TheInput(
-  {
-    field: { name, value, onChange, onBlur },
-    form: { touched, errors },
-    placeholder = '',
-    className = '',
-    title = '',
-    containerStyle = {},
-    inputStyle = {},
-    labelStyle = {},
-    children = InputChildren,
-    disabled = false,
-    autoFocus = false
-  },
-  inputRef: any
-) {
-  const [focused, setFocused] = useState(autoFocus)
-  const localRef = useRef(undefined)
+export const InputCreator = (
+  InputWrapperCmp: ReactComponentLike = InputWrapper,
+  InputFieldWrapperCmp: ReactComponentLike = InputFieldWrapper,
+  InputErrorMessageCmp: ReactComponentLike = InputErrorMessage
+) =>
+  function TheInput(
+    {
+      field: { name, value, onChange, onBlur },
+      form: { touched, errors },
+      placeholder = '',
+      className = '',
+      title = '',
+      containerStyle = {},
+      inputStyle = {},
+      labelStyle = {},
+      children = InputChildren,
+      disabled = false,
+      autoFocus = false
+    },
+    inputRef: any
+  ) {
+    const [focused, setFocused] = useState(autoFocus)
+    const localRef = useRef(undefined)
 
-  const ref = inputRef || localRef
+    const ref = inputRef || localRef
 
-  const handleClick = () => inputRef && inputRef.current.focus()
-  const hasErrors = touched[name] && errors[name]
-  return (
-    <InputFieldWrapper
-      onClick={handleClick}
-      className={className}
-      style={containerStyle}
-    >
-      <div style={labelStyle}>{!!title && <H3>{title}</H3>}</div>
-      <InputErrorWrapper>
-        <InputWrapper
-          style={inputStyle}
-          focused={focused}
-          hasErrors={hasErrors}
-        >
-          {children({
-            ref,
-            name,
-            value,
-            disabled,
-            type: 'text',
-            placeholder,
-            onFocus: () => setFocused(true),
-            onBlur: event => {
-              setFocused(false)
-              onBlur(event)
-            },
-            onChange: onChange,
-            autoFocus: autoFocus
-          })}
-        </InputWrapper>
-        {!!hasErrors && <InputErrorMessage>{errors[name]}</InputErrorMessage>}
-      </InputErrorWrapper>
-    </InputFieldWrapper>
-  )
-}
+    const handleClick = () => inputRef && inputRef.current.focus()
+    const hasErrors = touched[name] && errors[name]
+    return (
+      <InputFieldWrapperCmp
+        onClick={handleClick}
+        className={className}
+        style={containerStyle}
+      >
+        <div style={labelStyle}>{!!title && <H3>{title}</H3>}</div>
+        <InputErrorWrapper>
+          <InputWrapperCmp
+            style={inputStyle}
+            focused={focused}
+            hasErrors={hasErrors}
+          >
+            {children({
+              ref,
+              name,
+              value,
+              disabled,
+              type: 'text',
+              placeholder,
+              onFocus: () => setFocused(true),
+              onBlur: event => {
+                setFocused(false)
+                onBlur(event)
+              },
+              onChange: onChange,
+              autoFocus: autoFocus
+            })}
+          </InputWrapperCmp>
+          {!!hasErrors && (
+            <InputErrorMessageCmp>{errors[name]}</InputErrorMessageCmp>
+          )}
+        </InputErrorWrapper>
+      </InputFieldWrapperCmp>
+    )
+  }
 
-export default forwardRef(TheInput)
+export default forwardRef(
+  InputCreator(InputWrapper, InputFieldWrapper, InputErrorMessage)
+)
