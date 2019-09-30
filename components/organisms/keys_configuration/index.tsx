085f6f72f1fe875e250af25ca34e5bc960e68bdb
Reinaldo Viera <reinaldo@Reinaldos-MBP.fritz.box>
2019-09-30 13:28:06 +0200

    New designs for account screen

--- components/organisms/keys_configuration/index.tsx
+++ components/organisms/keys_configuration/index.tsx
@@ -3,22 +3,25 @@ import { Formik, Field } from 'formik'
 
 import {
   H3,
-  KeyIcon,
+  KeyIcon1,
   CopyIcon,
-  DeleteIcon,
+  Trash,
   Body,
   PrimaryButton,
   WarnButton,
-  SecondaryButton
+  SecondaryButton,
+  H2,
+  Add
 } from '../../atoms'
 import { Popup, Input } from '../../molecules'
 import { useLocalization, useToast } from '../../../hooks'
-import PageForm from '../page_form'
 import copyToClipboard from './copy_to_clipboard'
 import {
   SecretKeyWrapper,
   PublishableKeyWrapper,
   PopupWrapper,
+  Wrapper,
+  Header,
   KeysWrapper
 } from './styled'
 
@@ -49,73 +52,120 @@ export default function KeysConfiguration({
   const secretKeys = keys.filter(item => item.type === 'SECRET')
 
   return (
-    <PageForm title={getText('Keys')}>
+    <Wrapper>
+      <H2>{getText('Keys')}</H2>
+
       <div>
-        <H3>{getText('Publishable Keys')}</H3>
+        <Header>
+          <span className="title">{getText('Publishable Keys')}</span>
+          <PrimaryButton
+            type="button"
+            style={{ float: 'right' }}
+            label={''}
+            onClick={() => onCreate('PUBLISHABLE')}
+          >
+            <span className="add-plus">
+              <Add
+                style={{
+                  marginRight: '8px',
+                  transform: 'translate(-5px, 1px)'
+                }}
+              />
+              {getText('Create a new publishable Key')}
+            </span>
+          </PrimaryButton>
+        </Header>
         <KeysWrapper>
           {publishableKeys.length === 0 ? (
             <Body>{getText('there is no publishable key generated yet')}</Body>
           ) : (
             publishableKeys.map(keyEntity => (
               <PublishableKeyWrapper key={keyEntity.id}>
-                <KeyIcon />
-                <Body>{keyEntity.key}</Body>
-                <CopyIcon
-                  onClick={() => {
-                    copyToClipboard(keyEntity.key)
-                    toastSuccess(getText('Key Copied'))
-                  }}
+                <KeyIcon1
+                  width={32}
+                  height={32}
+                  className="circle"
+                  fill="#2e7ef2"
+                  stroke="#2e7ef2"
                 />
-                <DeleteIcon onClick={() => setDeleteKey(keyEntity)} />
+                <Body>{keyEntity.key}</Body>
+                <div className="action-container">
+                  <CopyIcon
+                    onClick={() => {
+                      copyToClipboard(keyEntity.key)
+                      toastSuccess(getText('Key Copied'))
+                    }}
+                  />
+                  <Trash hoverColor onClick={() => setDeleteKey(keyEntity)} />
+                </div>
               </PublishableKeyWrapper>
             ))
           )}
         </KeysWrapper>
-        <PrimaryButton
-          type="button"
-          label={getText('Create a new publishable Key')}
-          onClick={() => onCreate('PUBLISHABLE')}
-        />
       </div>
       <div style={{ marginTop: '32px' }}>
-        <H3>{getText('Secret Keys')}</H3>
+        <Header>
+          <span className="title">{getText('Secret Keys')}</span>
+          <PrimaryButton
+            type="button"
+            style={{ float: 'right' }}
+            label={getText('Create a new secret Key')}
+            onClick={() => setIsCreating(true)}
+          >
+            <span className="add-plus">
+              <Add
+                style={{
+                  marginRight: '8px',
+                  transform: 'translate(-5px, 1px)'
+                }}
+              />
+              {getText('Create a new secret Key')}
+            </span>
+          </PrimaryButton>
+        </Header>
         <KeysWrapper>
           {secretKeys.length === 0 ? (
             <Body>{getText('there is no secret key generated yet')}</Body>
           ) : (
             secretKeys.map(keyEntity => (
               <SecretKeyWrapper key={keyEntity.id}>
-                <KeyIcon />
+                <KeyIcon1
+                  width={32}
+                  height={32}
+                  className="circle"
+                  fill="#2e7ef2"
+                  stroke="#2e7ef2"
+                />
                 <div className="wrapper">
                   <div className="name">
                     <Body>{keyEntity.name}</Body>
-                    <DeleteIcon onClick={() => setDeleteKey(keyEntity)} />
+                    <div className="action-container">
+                      <Trash
+                        hoverColor
+                        onClick={() => setDeleteKey(keyEntity)}
+                      />
+                    </div>
                   </div>
                   {keyEntity.key && (
                     <div className="key">
                       <Body>{keyEntity.key}</Body>
-                      <CopyIcon
-                        onClick={() => copyToClipboard(keyEntity.key)}
-                      />
+                      <Body className="instruction">
+                        {getText(
+                          'Copy this key now because it cannot be recovered in the future.'
+                        )}
+                      </Body>
+                      <div className="action-container">
+                        <CopyIcon
+                          onClick={() => copyToClipboard(keyEntity.key)}
+                        />
+                      </div>
                     </div>
                   )}
                 </div>
-                {keyEntity.key && (
-                  <Body className="instruction">
-                    {getText(
-                      'Copy this key now because it cannot be recovered in the future.'
-                    )}
-                  </Body>
-                )}
               </SecretKeyWrapper>
             ))
           )}
         </KeysWrapper>
-        <PrimaryButton
-          type="button"
-          label={getText('Create a new secret Key')}
-          onClick={() => setIsCreating(true)}
-        />
       </div>
       <Popup show={!!deleteKey} onClose={() => setDeleteKey(undefined)}>
         <PopupWrapper>
@@ -188,6 +238,6 @@ export default function KeysConfiguration({
           )}
         />
       </Popup>
-    </PageForm>
+    </Wrapper>
   )
 }
--- components/organisms/keys_configuration/styled.tsx
+++ components/organisms/keys_configuration/styled.tsx
@@ -1,8 +1,49 @@
 import styled from '../../styled'
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: ${p => p.theme.spacing.xxlarge};
+  margin-left: ${p => p.theme.spacing.xxxlarge};
+  margin-right: ${p => p.theme.spacing.xxxlarge};
+  margin-bottom: 0;
+`
+
+export const Header = styled.div`
+  display: block;
+  height: 38px;
+  .title {
+    font-size: 16px;
+    font-weight: bold;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 2.44;
+    letter-spacing: normal;
+    color: #12202a;
+  }
+`
+
 export const KeysWrapper = styled.div`
   margin-top: ${p => p.theme.spacing.small};
   margin-bottom: ${p => p.theme.spacing.small};
+  .circle {
+    background-color: ${p => p.theme.primary.A100};
+    border: 8px solid ${p => p.theme.primary.A100};
+    border-radius: 50%;
+  }
+  .action-container {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 8px;
+    > svg {
+      cursor: pointer;
+    }
+    > svg:last-child {
+      margin-left: ${p => p.theme.spacing.medium};
+    }
+  }
 `
 
 export const PublishableKeyWrapper = styled.div`
@@ -10,17 +51,16 @@ export const PublishableKeyWrapper = styled.div`
   margin-bottom: ${p => p.theme.spacing.small};
   display: flex;
   align-items: center;
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.06);
+  background-color: #ffffff;
+
   > span {
     margin-left: ${p => p.theme.spacing.medium};
     margin-right: ${p => p.theme.spacing.medium};
     display: block;
     min-width: 265px;
-  }
-  > svg:not(:first-child) {
-    cursor: pointer;
-  }
-  > svg:last-child {
-    margin-left: ${p => p.theme.spacing.medium};
   }
 `
 
@@ -49,6 +89,10 @@ export const SecretKeyWrapper = styled.div`
   margin-bottom: ${p => p.theme.spacing.small};
   display: flex;
   align-items: center;
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.06);
+  background-color: #ffffff;
   .name,
   .key {
     display: flex;
@@ -62,17 +106,19 @@ export const SecretKeyWrapper = styled.div`
     display: block;
     min-width: 265px;
   }
-  .name > svg,
-  .key > svg {
+  .name svg,
+  .key svg {
     cursor: pointer;
     margin-left: ${p => p.theme.spacing.medium};
   }
   .wrapper {
     display: flex;
     flex-direction: column;
+    width: 100%;
   }
   .instruction {
     display: block;
     margin-left: ${p => p.theme.spacing.medium};
+    color: ${p => p.theme.shade.A200};
   }
 `
